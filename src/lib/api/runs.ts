64affddad0f95ace08acb8cc04f19687822ccf41
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-22 07:48:03 +0700

    fix(typecheck): use default reader result type

--- src/lib/api/runs.ts
+++ src/lib/api/runs.ts
@@ -71,7 +71,7 @@ class StreamStalled extends Error {
 
 async function readWithStallTimeout<T>(
   reader: ReadableStreamDefaultReader<T>,
-): Promise<ReadableStreamReadResult<T>> {
+): Promise<ReadableStreamDefaultReadResult<T>> {
   let timer: ReturnType<typeof setTimeout> | undefined
   try {
     return await Promise.race([
